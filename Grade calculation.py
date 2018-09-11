c1f9ce5d3a56d0dc6adb444b7ef5fd6876e581ad
Oluchi0808 <43132928+Oluchi0808@users.noreply.github.com>
2018-09-11 09:09:04 +0100

    Add files via upload

---
+++ Grade calculation.py
@@ -0,0 +1,23 @@
+#write a program on grade calculation
+x= int(input("input a number "))
+if(100>=x>=70):
+    print("your grade is A")
+        
+elif (69>=x>=60):
+    print("your grade is B")
+        
+elif (59>=x>=50):
+    print("your grade is C")
+            
+elif (49>=x>=45):
+    print("your grade is D")
+
+elif (44>=x>=40):
+    print("your grade is E")
+
+elif (39>=x>=0):
+    print("your grade is F")
+
+else:
+    print("Marks are between 0 and 100")
+    
